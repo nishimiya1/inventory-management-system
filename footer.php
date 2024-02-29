120fc3030763f9ee7e9f477a9e229f477d932094
nishimiya1 <geralddiongzon12@gmail.com>
2024-02-29 23:46:03 +0800

    Edit href link

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
     <p>Gerald Angelo Diongzon</p>
     <div class="d-flex gap-4 gap-sm-9 position-relative lg-absolute lg-left-50 lg-translate-middle-x">
-        <a href="https://github.com/nishimiya1/Inventory-system" target="_blank" rel="noopener noreferrer"
+        <a href="https://github.com/nishimiya1/inventory-management-system" target="_blank" rel="noopener noreferrer"
             style="text-decoration:none;" class="text-muted">Github</a>
         <a href="https://www.linkedin.com/in/gerald-angelo-diongzon/" target="_blank" rel="noopener noreferrer"
             style="text-decoration:none;" class="text-muted">LinkedIn</a>
